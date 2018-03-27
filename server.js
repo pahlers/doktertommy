786db971ac20b79693906ec049dbdb6e4fa8e63b
Peter Ahlers <peter.ahlers@gmail.com>
2018-03-27 20:06:09 +0200

    feat(server): Set default port to 3001

--- server.js
+++ server.js
@@ -4,6 +4,6 @@
 
 const server = require('./');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 server.listen(port, console.log(`Server listens on port ${port}`));

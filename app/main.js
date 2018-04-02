fed47795bc0459e920daec8ae7ae3fed0ba9b5a3
Peter Ahlers <peter.ahlers@gmail.com>
2018-04-02 09:44:12 +0200

    feat(settings): Add settings with backup and notifications

--- app/main.js
+++ app/main.js
@@ -1,15 +1,24 @@
 (function () {
     console.log('[doktertommy] run');
 
-    const storageKey = 'dokterTommyHistoryList';
+    const settingsStorageKey = `dokterTommySettings`;
+    const historyStorageKey = `dokterTommyHistoryList`;
 
-    init();
+    enableEvents();
+    enableServiceWorker();
 
-    function init() {
-        enableEvents();
-        enableServiceWorker();
-        fillHistoryFromStorage();
-    }
+    const defaultSettings = {
+        notification: {
+            enabled: false,
+            time: {
+                hours: 18,
+                minutes: 0
+            }
+        }
+    };
+
+    renderSettings(getSettingsFromStorage());
+    renderHistory(getHistoryFromStorage());
 
     function enableEvents() {
         console.log('[doktertommy] enable events');
@@ -19,6 +28,15 @@
 
         document.querySelector('.save-button')
             .addEventListener('click', saveThisDay);
+
+        document.querySelector('.settings-button')
+            .addEventListener('click', toggleQuestionsAndSettings);
+
+        document.querySelector('.leave-settings-button')
+            .addEventListener('click', leaveSettings);
+
+        Array.from(document.querySelectorAll('.notification-status input'))
+            .map(element => element.addEventListener('click', requestNotificationPermission));
     }
 
     function enableServiceWorker() {
@@ -32,6 +50,7 @@
             })
                 .then(function (data) {
                     console.log(`[doktertommy] server worker started`, data);
+
                 })
                 .catch(function (error) {
                     console.error(`[doktertommy] server worker didn't work`, error);
@@ -40,6 +59,47 @@
         } else {
             console.warn(`[doktertommy] server worker isn't supported by browser`);
         }
+    }
+
+    function leaveSettings() {
+        storeSettings();
+        toggleQuestionsAndSettings();
+    }
+
+    function storeSettings() {
+        const enabled = document.querySelector('.settings .notification-status input[value="on"]').checked;
+        const hours = parseInt(document.querySelector('.settings .notification-time-hours').value, 10) || 0;
+        const minutes = parseInt(document.querySelector('.settings .notification-time-minutes').value, 10) || 0;
+
+        const settings = Object.assign({}, getSettingsFromStorage(), {
+            notification: {
+                enabled,
+                time: {
+                    hours,
+                    minutes
+                }
+            }
+        });
+
+        if ('serviceWorker' in navigator) {
+            console.log('[doktertommy] send to serviceworker');
+
+            navigator.serviceWorker.controller.postMessage({
+                type: 'updateSettings',
+                data: settings
+            });
+        }
+
+        console.log('[doktertommy] store settings', JSON.stringify(settings, null, 2));
+
+        localStorage.setItem(settingsStorageKey, JSON.stringify(settings));
+    }
+
+    function toggleQuestionsAndSettings() {
+        console.log('[doktertommy] toggle questions and settings');
+
+        document.querySelector('.questions').classList.toggle('hidden');
+        document.querySelector('.settings').classList.toggle('hidden');
     }
 
     function generateDateTimeString() {
@@ -79,11 +139,19 @@
         cleanForm();
         addToHistoryList(day);
 
-        saveInStorage(day);
+        setHistoryInStorage(day);
     }
 
-    function saveInStorage(day) {
-        let list = JSON.parse(localStorage.getItem(storageKey));
+    function getSettingsFromStorage() {
+        return localStorage[settingsStorageKey] !== undefined ? JSON.parse(localStorage.getItem(settingsStorageKey)) : defaultSettings;
+    }
+
+    function getHistoryFromStorage() {
+        return JSON.parse(localStorage.getItem(historyStorageKey));
+    }
+
+    function setHistoryInStorage(day) {
+        let list = JSON.parse(localStorage.getItem(historyStorageKey));
 
         if (list === null) {
             list = [];
@@ -91,16 +159,14 @@
 
         list.push(day);
 
-        localStorage.setItem(storageKey, JSON.stringify(list));
+        localStorage.setItem(historyStorageKey, JSON.stringify(list));
     }
 
-    function fillHistoryFromStorage() {
+    function renderHistory(history) {
         console.log('[doktertommy] fill history from storage');
 
-        let list = JSON.parse(localStorage.getItem(storageKey));
-
-        if (list !== null) {
-            list.forEach(addToHistoryList);
+        if (history !== null) {
+            history.forEach(addToHistoryList);
         }
     }
 
@@ -152,5 +218,87 @@
         }
 
         list.insertBefore(clone, list.firstChild);
+    }
+
+    function renderSettings(settings) {
+        console.log('[doktertommy] render settings');
+
+        if ('serviceWorker' in navigator) {
+            document.querySelector('.settings .notification').classList.remove('hidden');
+
+            if (settings !== null && settings.notification !== undefined) {
+                console.log('[doktertommy] render notification settings');
+                const {notification: {enabled, time}} = settings;
+
+                if (enabled === true) {
+                    document.querySelector('.settings .notification-status input[value="on"]').setAttribute('checked', 'true');
+                    document.querySelector('.settings .notification-status input[value="off"]').removeAttribute('checked');
+                } else {
+                    document.querySelector('.settings .notification-status input[value="on"]').removeAttribute('checked');
+                    document.querySelector('.settings .notification-status input[value="off"]').setAttribute('checked', 'true');
+                }
+
+                if (time !== undefined) {
+                    document.querySelector('.settings .notification-time-hours').value = ('' + time.hours).padStart(2, '0');
+                    document.querySelector('.settings .notification-time-minutes').value = ('' + time.minutes).padStart(2, '0');
+                }
+            }
+        }
+
+        const dataElement = document.querySelector('.settings .data');
+        const data = getHistoryFromStorage();
+        dataElement.value = JSON.stringify(data, null, 2);
+    }
+
+    function requestNotificationPermission(event) {
+        if (event.target.value === 'on') {
+            // Enable notifications
+            if (Notification.permission === 'default') {
+                console.log('[doktertommy] request for notification');
+
+                Notification.requestPermission(function (result) {
+                    if (result === 'granted') {
+                        console.log('[doktertommy] allow notification');
+
+                        triggerTestNotification();
+
+                    } else if (result === 'denied') {
+                        console.log('[doktertommy] block notification');
+
+                        toggleNotificationPermissionError();
+
+                    } else {
+                        console.log('[doktertommy] ignore message notification');
+
+                        toggleNotificationPermissionError();
+                    }
+                });
+            } else if (Notification.permission === 'granted') {
+                triggerTestNotification();
+
+            } else {
+                // Notification permission is denied
+
+                toggleNotificationPermissionError();
+            }
+
+        } else {
+            // Disable notifications
+            document.querySelector('.notification-permission-failed').classList.add('hidden');
+        }
+    }
+
+    function toggleNotificationPermissionError() {
+        document.querySelector('.notification-permission-failed').classList.toggle('hidden');
+    }
+
+    function triggerTestNotification() {
+        navigator.serviceWorker.ready.then(function (registration) {
+            registration.showNotification('Dokter Tommy', {
+                body: 'Hoe is je dag?',
+                icon: './images/192x192.png',
+                tag: 'how-is-your-day',
+            });
+        });
     }
 })();
--- app/service-worker.js
+++ app/service-worker.js
@@ -1,125 +1,203 @@
-console.log('[doktertommy sw] run');
+(function () {
+    console.log('[doktertommy sw] run');
 
-/**
- * Key of cache used cache
- *
- * @type {string}
- */
-const cacheKey = 'doktertommy-static-v1';
+    /**
+     * Key of cache used cache
+     *
+     * @type {string}
+     */
+    const cacheKey = 'doktertommy-static-v1';
 
-/**
- * Cache sources at install
- *
- * @type {string[]}
- */
-const cacheList = [
-    '/',
-    '/main.css',
-    '/main.js',
-    '/manifest.json'
-];
+    /**
+     * Cache sources at install
+     *
+     * @type {string[]}
+     */
+    const cacheList = [
+        '/',
+        '/main.css',
+        '/main.js',
+        '/manifest.json'
+    ];
 
-/**
- * Whitelist of sources to cache at network fetch
- *
- * @type {string[]}
- */
-const cacheWhitelist = [
-    '/muicss/*',
-    '/weather-icons/*',
-    '/images/*',
-    ...cacheList
-];
 
-self.addEventListener('install', oninstall.bind({cacheList, cacheKey}));
-self.addEventListener('fetch', onfetch.bind({cacheWhitelist, cacheKey}));
+    let timeoutId;
 
-/**
- * Triggers on install
- *
- * @param event
- */
-function oninstall(event) {
-    const {cacheList, cacheKey} = this;
-    console.log('[doktertommy sw] install');
+    /**
+     * Whitelist of sources to cache at network fetch
+     *
+     * @type {string[]}
+     */
+    const cacheWhitelist = [
+        '/muicss/*',
+        '/weather-icons/*',
+        '/images/*',
+        ...cacheList
+    ];
 
-    event.waitUntil(
-        caches.open(cacheKey).then(function (cache) {
-            return cache.addAll(cacheList)
-                .then(() => console.debug('[doktertommy sw] all cached', cacheList))
-                .catch(error => console.warn('[doktertommy sw] cache failed', error));
-        })
-    );
-}
+    self.addEventListener('install', onInstall.bind({cacheList, cacheKey}));
+    self.addEventListener('fetch', onFetch.bind({cacheWhitelist, cacheKey}));
+    self.addEventListener('message', onMessage);
+    self.addEventListener('notificationclick', onNotificationClick);
 
-/**
- * Triggers on fetch
- *
- * @param event
- */
-function onfetch(event) {
-    const {cacheWhitelist, cacheKey} = this;
-    const request = event.request;
+    function onMessage(event) {
+        const {type, data} = event.data;
 
-    console.log(`[doktertommy sw] fetch ${request.url}`);
+        console.log(`[doktertommy sw] got message from type ${type}`);
 
-    event.respondWith(
-        caches.open(cacheKey)
-            .then(function (cache) {
-                return cache.match(request)
-                    .then(function (response) {
-                        console.debug(`[doktertommy sw] fetched cache ${request.url}`);
+        if (type === 'updateSettings') {
+            setNotificationTimeout(data.notification);
+        }
+    }
 
-                        const updatePromise = getFromNetwork(request, cacheWhitelist, cache);
+    /**
+     * Triggers on install
+     *
+     * @param event
+     */
+    function onInstall(event) {
+        const {cacheList, cacheKey} = this;
+        console.log('[doktertommy sw] install');
 
-                        return response || updatePromise;  // return from cache, otherwise get from network
-                    })
+        event.waitUntil(
+            caches.open(cacheKey).then(function (cache) {
+                return cache.addAll(cacheList)
+                    .then(() => console.debug('[doktertommy sw] all cached', cacheList))
+                    .catch(error => console.warn('[doktertommy sw] cache failed', error));
             })
-    );
-}
+        );
+    }
 
-/**
- * Gets source from network and caches if needed.
- *
- * @param {Request} request
- * @param {array} cacheWhitelist
- * @param {Cache} cache
- * @return {Promise<Response>}
- */
-function getFromNetwork(request, cacheWhitelist, cache) {
-    return fetch(request)
-        .then(function (networkResponse) {
-            console.debug(`[doktertommy sw] fetched network ${request.url}`);
+    /**
+     * Triggers on fetch
+     *
+     * @param event
+     */
+    function onFetch(event) {
+        const {cacheWhitelist, cacheKey} = this;
+        const request = event.request;
 
-            if (needToBeCached(request.url, cacheWhitelist)) {
-                console.debug(`[doktertommy sw] add to cache ${request.url}`);
+        console.log(`[doktertommy sw] fetch ${request.url}`);
 
-                cache.put(request, networkResponse.clone());
+        event.respondWith(
+            caches.open(cacheKey)
+                .then(function (cache) {
+                    return cache.match(request)
+                        .then(function (response) {
+                            console.debug(`[doktertommy sw] fetched cache ${request.url}`);
+
+                            const updatePromise = getFromNetwork(request, cacheWhitelist, cache);
+
+                            return response || updatePromise;  // return from cache, otherwise get from network
+                        })
+                })
+        );
+    }
+
+    /**
+     * Gets source from network and caches if needed.
+     *
+     * @param {Request} request
+     * @param {array} cacheWhitelist
+     * @param {Cache} cache
+     * @return {Promise<Response>}
+     */
+    function getFromNetwork(request, cacheWhitelist, cache) {
+        return fetch(request)
+            .then(function (networkResponse) {
+                console.debug(`[doktertommy sw] fetched network ${request.url}`);
+
+                if (needToBeCached(request.url, cacheWhitelist)) {
+                    console.debug(`[doktertommy sw] add to cache ${request.url}`);
+
+                    cache.put(request, networkResponse.clone());
+                }
+
+                return networkResponse;
+            })
+            .catch(function (error) {
+                console.error(`[doktertommy sw] fetch from network failed ${request.url} error ${error}`);
+            });
+    }
+
+    /**
+     * Returns true source needs to be cached.
+     *
+     * @param {string} url
+     * @param {array} cacheWhitelist
+     * @return {boolean}
+     */
+    function needToBeCached(url, cacheWhitelist) {
+        const path = new URL(url).pathname;
+
+        return cacheWhitelist.some(function (cacheRule) {
+            if (cacheRule.endsWith('*')) {
+                return path.startsWith(cacheRule.slice(0, -1));
+
+            } else {
+                return path === cacheRule;
+            }
+        });
+    }
+
+    function onNotificationClick(event) {
+        console.log('[doktertommy sw] on notification click', event.notification.tag);
+        event.notification.close();
+
+        // This looks to see if the current is already open and
+        // focuses if it is
+        event.waitUntil(clients.matchAll({
+            type: 'window'
+        }).then(function (clientList) {
+            const client = clientList.find(client => (client.url === '/' && 'focus' in client));
+
+            if (client !== undefined) {
+                return client.focus();
             }
 
-            return networkResponse;
-        })
-        .catch(function (error) {
-            console.error(`[doktertommy sw] fetch from network failed ${request.url} error ${error}`);
-        });
-}
+            if (clients.openWindow) {
+                return clients.openWindow('/');
+            }
+        }));
+    }
 
-/**
- * Returns true source needs to be cached.
- *
- * @param {string} url
- * @param {array} cacheWhitelist
- * @return {boolean}
- */
-function needToBeCached(url, cacheWhitelist) {
-    const path = new URL(url).pathname;
+    function setNotificationTimeout(settings) {
+        const {enabled, time: {hours, minutes}} = settings;
+        const currentTime = Date.now();
 
-    return cacheWhitelist.some(function (cacheRule) {
-        if (cacheRule.endsWith('*')) {
-            return path.startsWith(cacheRule.slice(0, -1));
+        let notificationTime = new Date();
+        notificationTime.setHours(hours);
+        notificationTime.setMinutes(minutes);
 
-        } else {
-            return path === cacheRule;
+
+        if(notificationTime < currentTime){
+            notificationTime.setDate(notificationTime.getDate() + 1);
+
+            console.log(`[doktertommy sw] date was in the past. Add one day. ${notificationTime}`);
         }
-    });
-}
+
+        const delay = notificationTime - currentTime;
+
+        if (timeoutId !== undefined) {
+            clearTimeout(timeoutId);
+            timeoutId = undefined;
+        }
+
+        if (enabled === true) {
+            console.log(`[doktertommy sw] set timer for notification over ${delay}ms`);
+
+            timeoutId = setTimeout(    function sendNotification() {
+                console.log('[doktertommy sw] show notification');
+
+
+                self.registration.showNotification('Dokter Tommy', {
+                    body: 'Hoe is je dag?',
+                    icon: './images/192x192.png',
+                    tag: 'how-is-your-day',
+                });
+            }, delay);
+        }
+    }
+
+
+})();
